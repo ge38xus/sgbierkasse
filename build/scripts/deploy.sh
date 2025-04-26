06f5fca0ea83db6cc74f2a8aeb67709360738bd5
ge38xus <66480850+ge38xus@users.noreply.github.com>
2025-04-26 14:04:41 +0200

    [TC] AWS Deploy appspec

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -1,3 +1,4 @@
-cd /home/ec2-user/bierkasse
+cd /home/ec2-user/bierkasse || echo "wrong dir"; exit 1
 docker image build -t docker-java-jar:latest .
 docker run -p 8080:8080 docker-java-jar:latest
+exit 0
