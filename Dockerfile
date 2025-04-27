071054318fedbc7afd7af35a46b074d549e18401
ge38xus <66480850+ge38xus@users.noreply.github.com>
2025-04-27 11:12:48 +0200

    [TC] AWS Build images and stop dockerapps before

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:19
 COPY target/Bierkasse-0.0.1-SNAPSHOT.jar app.jar
-COPY src/main/resources ./resources
+COPY src/main/resources /resources
 COPY env.properties env.properties
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- build/scripts/buildDockerForAWS.sh
+++
@@ -1,9 +0,0 @@
-ECR_URI="383397309571.dkr.ecr.eu-north-1.amazonaws.com/prezzesik/bierkasse:latest"
-IMAGE_NAME="prezzesik/bierkasse"
-
-echo "🐳 Building Docker image..."
-docker build --provenance false -t ${IMAGE_NAME} .
-docker tag ${IMAGE_NAME}:latest ${ECR_URI}
-echo "📦 Pushing image to ECR..."
-docker push ${ECR_URI}
-echo "✅ Image successfully pushed to ${ECR_URI}"
